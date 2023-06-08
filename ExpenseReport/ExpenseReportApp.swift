736e7203243397f415cdd9b52cf0c093c0aa1ea3
Jeroen Abrahams <jeroen.abrahams@aviobook.aero>
2023-06-08 13:16:23 +0200

    Initial Commit

---
+++ ExpenseReport/ExpenseReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseReportApp.swift
+//  ExpenseReport
+//
+//  Created by Jeroen Abrahams on 08/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
